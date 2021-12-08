475d40814e0ad269501c9fbfffd85af9715ba9c0
diegonicita <diegonicita@gmail.com>
2021-12-08 10:29:36 -0300

    corrigiendo el .sql

--- solocalzado_db.sql
+++ solocalzado_db.sql
@@ -8,6 +8,7 @@ CREATE TABLE `users` (
    `name` VARCHAR(100) NOT NULL,
    `email` VARCHAR(100) NOT NULL,
    `password` VARCHAR(100) NOT NULL,
+   `avatar` VARCHAR(100) DEFAULT NULL,
    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -17,11 +18,7 @@ CREATE TABLE `products` (
   `name` varchar(200) DEFAULT NULL,
   `description` text DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
-  `image` varchar(300) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `offer` tinyint(4) DEFAULT '0',
+  `image` varchar(300) DEFAULT NULL,  
    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -29,6 +26,7 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
    `id` INT NOT NULL AUTO_INCREMENT,
    `name` VARCHAR(100) NOT NULL,
+   `description` text DEFAULT NULL,
    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -41,7 +39,22 @@ CREATE TABLE `carts` (
    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`, `created_at`, `deleted_at`, `updated_at`, `offer`) VALUES (null, 'Zapatilla Generica 1', 'Zapatilla muy Generica numero 1', '1529', NULL, NULL, NULL, NULL, 15);
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`, `created_at`, `deleted_at`, `updated_at`, `offer`) VALUES (null, 'Zapatilla Generica 2', 'Zapatilla muy Generica numero 2', '2529', NULL, NULL, NULL, NULL, 5);
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`, `created_at`, `deleted_at`, `updated_at`, `offer`) VALUES (null, 'Zapatilla Generica 2', 'Zapatilla muy Generica numero 3', '3529', NULL, NULL, NULL, NULL, 25);
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`, `created_at`, `deleted_at`, `updated_at`, `offer`) VALUES (null, 'Zapatilla Generica 2', 'Zapatilla muy Generica numero 4', '4529', NULL, NULL, NULL, NULL, 0);
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `avatar`) VALUES (null, "user1", "user1@gmail.com", "123456", "default-avatar.png");
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `avatar`) VALUES (null, "user2", "user2@gmail.com", "123456", "default-avatar.png");
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `avatar`) VALUES (null, "user3", "user3@gmail.com", "123456", "default-avatar.png");
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `avatar`) VALUES (null, "user4", "user4@gmail.com", "123456", "default-avatar.png");
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`) VALUES (null, "Zapatilla Generica 1", "Zapatilla muy Generica numero 1", "1529", "zapatilla001.png");
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`) VALUES (null, "Zapatilla Generica 2", "Zapatilla muy Generica numero 2", "2529", "zapatilla002.png");
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`) VALUES (null, "Zapatilla Generica 3", "Zapatilla muy Generica numero 3", "3529", "zapatilla003.png");
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`) VALUES (null, "Zapatilla Generica 4", "Zapatilla muy Generica numero 4", "4529", "zapatilla004.png");
+
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES (null, "Categoria 1", "Esta es una categoria 1");
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES (null, "Categoria 2", "Esta es una categoria 2");
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES (null, "Categoria 3", "Esta es una categoria 3");
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES (null, "Categoria 4", "Esta es una categoria 4");
+
+INSERT INTO `carts` (`id`, `user_id`, `status`, `products`) VALUES (null, 1, 0, 0);
+INSERT INTO `carts` (`id`, `user_id`, `status`, `products`) VALUES (null, 2, 0, 0);
+INSERT INTO `carts` (`id`, `user_id`, `status`, `products`) VALUES (null, 3, 0, 0);
+INSERT INTO `carts` (`id`, `user_id`, `status`, `products`) VALUES (null, 4, 0, 0);
