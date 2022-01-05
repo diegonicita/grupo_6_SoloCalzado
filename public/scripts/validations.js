9ad75af29404c15f9b0435b85cc9eadb98bb99c4
EricMainhard <mainhard.eric@gmail.com>
2022-01-04 23:42:19 -0300

    Add for to validation

--- public/scripts/validations.js
+++ public/scripts/validations.js
@@ -1,19 +1,19 @@
-//LOGIN VALIDATION
+//REGISTER VALIDATION
 let $regForm = d.getElementsByClassName('reg-form')[0],
 inputs = $regForm.querySelectorAll('input'),
 $firstName = d.getElementById('firstName'),
 $lastName = d.getElementById('lastName') ,
 $user = d.getElementById('user'),
 $password = d.getElementById('regEmail'),
-$avatar = d.getElementById('avatar');
+$avatar = d.getElementById('avatar'),
+inputsArray = [$firstName,$lastName,$user,$password,$avatar];
 
-const checkField = (e)=>{
-    if (e.target === $firstName){
-        if($firstName.value.trim() == ''){
-            setError($firstName,'Debes completar el campo')
-        } else {
-            setSuccess($firstName)
-        }
+
+const checkField = (input) => {   
+    if (input.value.trim() == ''){
+        setError(input,'Debes completar el campo')
+    } else {
+        setSuccess(input)
     }
 }
 
@@ -36,8 +36,11 @@ const setSuccess = field => {
 };
 
 inputs.forEach(input => {
-  input.addEventListener('blur',checkField);  
+    input.addEventListener('blur', ()=>{
+    checkField(input)});  
 })
+
+
 
 //REGISTER VALIDATION
 
