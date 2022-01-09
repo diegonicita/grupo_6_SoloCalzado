582738b6347ec4af5d67547cd8dfe7a0a9592e87
diegonicita <diegonicita@gmail.com>
2022-01-09 18:10:51 -0300

    validando desde el backend los tipos de archivos permitidos

--- routes/users.js
+++ routes/users.js
@@ -1,85 +1,133 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-const multer = require('multer')
-const {body} = require('express-validator');
-const path = require('path');
-const controller = require('../controllers/usersController');
-const { userInfo } = require('os');
+const multer = require("multer");
+const { body, checkSchema } = require("express-validator");
+const path = require("path");
+const controller = require("../controllers/usersController");
+const { userInfo } = require("os");
 
 // MIDDLEWARES
 
-const userLevelAuthMiddleware = require('../middlewares/userLevelAuthMiddleware')
-const authMiddleware = require('../middlewares/authMiddleware');
-const guestMiddleware = require('../middlewares/guestMiddleware');
+const userLevelAuthMiddleware = require("../middlewares/userLevelAuthMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
+const guestMiddleware = require("../middlewares/guestMiddleware");
 
 // Preparando el login para recibir imágenes
 
 const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,'./public/images/avatars')
-    },
-    filename:function(req,file,cb){
-        let filename = `avatar-${Date.now()}-img-${path.extname(file.originalname)}`;
-        cb(null,filename);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/avatars");
+  },
+  filename: function (req, file, cb) {
+    let filename = `avatar-${Date.now()}-img-${path.extname(
+      file.originalname
+    )}`;
+    cb(null, filename);
+  },
+});
 
-const upload = multer({storage});
+const upload = multer({
+  storage: storage
+});
 
-// Validaciones login 
+// Validaciones login
 const loginValidations = [
-	body('usuario').notEmpty().withMessage('Debes completar el campo'),
-	body('password').notEmpty().withMessage('Debes completar el campo')
+  body("usuario").notEmpty().withMessage("Debes completar el campo"),
+  body("password").notEmpty().withMessage("Debes completar el campo"),
 ];
 const registerValidations = [
-    body('firstName')
-        .notEmpty().withMessage('Debes completar el campo')
-        .isLength({min:2,max:undefined}).withMessage('El nombre debe contener al menos 2 letras'),
-    body('lastName').
-        notEmpty().withMessage('Debes completar el campo')
-        .isLength({min:2,max:undefined}).withMessage('El apellido debe contener al menos 2 letras'),
-    body('email')
-        .notEmpty().withMessage('Debes completar el campo')
-        .isEmail().withMessage('Debes ingresar un formato de email válido'),
-    body('user').notEmpty().withMessage('Debes completar el campo'),
-    body('password')
-        .notEmpty().withMessage('Debes completar el campo')
-        .isLength({min:8,max:16}).withMessage('La contraseña debe contener entre 8 y 20 caracteres'),
-    body('avatar')
-        .custom(file=>{
-            let fileType = path.extname(file.originalname);
-            if (fileType != '.png' || fileType != '.jpg' || fileType != '.jpeg' || fileType != '.gif' ){
-                throw new Error('Debes ingresar un archivo de tipo jpg, jpeg, gif o png');
-            }
-        })
-]
+  body("firstName")
+    .notEmpty()
+    .withMessage("Debes completar el campo")
+    .isLength({ min: 2, max: undefined })
+    .withMessage("El nombre debe contener al menos 2 letras"),
+  body("lastName")
+    .notEmpty()
+    .withMessage("Debes completar el campo")
+    .isLength({ min: 2, max: undefined })
+    .withMessage("El apellido debe contener al menos 2 letras"),
+  body("email")
+    .notEmpty()
+    .withMessage("Debes completar el campo")
+    .isEmail()
+    .withMessage("Debes ingresar un formato de email válido"),
+  body("user").notEmpty().withMessage("Debes completar el campo"),
+  body("password")
+    .notEmpty()
+    .withMessage("Debes completar el campo")
+    .isLength({ min: 8, max: 16 })
+    .withMessage("La contraseña debe contener entre 8 y 20 caracteres"),
+  body("avatar").custom((value, { req }) => {
+    if (req.file != undefined)
+    {
+    let file = req.file;
+    let extensionsAllowed = [".jpeg", ".jpg", ".png", ".gif"];   
+    let fileExtension = path.extname(file.originalname);
+    if (!extensionsAllowed.includes(fileExtension)) {
+        throw new Error("Solo puedes usar archivos " + extensionsAllowed.join(", "));
+        }    
+    }
+    return true;
+  }),
+];
+
 const updateUserValidations = [
-    body('avatar')
-        .custom(file=>{
-            let fileType = path.extname(file.originalname);
-            if (fileType != '.png' || fileType != '.jpg' || fileType != '.jpeg' || fileType != '.gif' ){
-                throw new Error('Debes ingresar un archivo de tipo jpg, jpeg, gif o png');
-            }
-        })
-]
+    body("avatar").custom((value, { req }) => {
+        if (req.file != undefined)
+        {
+        let file = req.file;
+        let extensionsAllowed = [".jpeg", ".jpg", ".png", ".gif"];   
+        let fileExtension = path.extname(file.originalname);
+        if (!extensionsAllowed.includes(fileExtension)) {
+            throw new Error("Solo puedes usar archivos " + extensionsAllowed.join(", "));
+            }    
+        }
+        return true;
+      }),  
+];
 // rutas para el login y el register
-router.get('/login', authMiddleware ,controller.login);
-router.post('/login',loginValidations,controller.processLogin);
+router.get("/login", authMiddleware, controller.login);
+router.post("/login", loginValidations, controller.processLogin);
 
-router.get('/register', authMiddleware , controller.register);
-router.post('/register', upload.single('avatar'), registerValidations ,controller.processRegister);
+router.get("/register", authMiddleware, controller.register);
+router.post(
+  "/register",
+  upload.single("avatar"),
+  registerValidations,
+  controller.processRegister
+);
 
-router.get('/profile', guestMiddleware, controller.profile);
-router.post('/update', guestMiddleware, upload.single('avatar'), updateUserValidations , controller.update);
+router.get("/profile", guestMiddleware, controller.profile);
+router.post(
+  "/update",
+  guestMiddleware,
+  upload.single("avatar"),
+  updateUserValidations,
+  controller.update
+);
 
-router.get('/list', guestMiddleware, userLevelAuthMiddleware({level : 3 }), controller.adminList);
-router.get('/edit/:id', guestMiddleware, userLevelAuthMiddleware({level : 3 }), controller.adminEdit);
-router.patch('/edit/:id', guestMiddleware, userLevelAuthMiddleware({level : 3 }), upload.single('avatar'), controller.adminUpdateById);
+router.get(
+  "/list",
+  guestMiddleware,
+  userLevelAuthMiddleware({ level: 3 }),
+  controller.adminList
+);
+router.get(
+  "/edit/:id",
+  guestMiddleware,
+  userLevelAuthMiddleware({ level: 3 }),
+  controller.adminEdit
+);
+router.patch(
+  "/edit/:id",
+  guestMiddleware,
+  userLevelAuthMiddleware({ level: 3 }),
+  upload.single("avatar"),
+  controller.adminUpdateById
+);
 
-router.get('/logout', controller.logout);
+router.get("/logout", controller.logout);
 
-router.get('/error', controller.error);
-
-
+router.get("/error", controller.error);
 
 module.exports = router;
